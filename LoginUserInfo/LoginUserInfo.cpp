977732fc6e13e7863ead5df46818a970e4909dcf
LeeHyunKyu <38107616+ngee044@users.noreply.github.com>
2021-04-27 14:46:02 +0900

    ini file 읽는 부분을 추가
    프로젝트에서 파일을 읽어 들여서 db에 들어올수있도록 코드를 추가함

--- LoginUserInfo/LoginUserInfo.cpp
+++ LoginUserInfo/LoginUserInfo.cpp
@@ -1,15 +1,31 @@
 #include <QDebug>
 #include <QtCore/QCoreApplication>
 #include <QNetworkAccessManager>
-
+#include <QSetting>
 #include "LoginUserInfo.h"
 
 LoginUserInfo::LoginUserInfo()
 {
+	QSetting setting("configure.ini", QSettings::IniFormat);
+	setting.beginGroup("database");
+	
+	QString name = setting.value("name").toString();
+	QString hostname = setting.value("hostname").toString();
+	QString pw = setting.value("password").toString();
+	int port = setting.value("port").toInt();
+
 	qDebug() << QSqlDatabase::drivers();
 	QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-
-	qDebug() << db.open();
-
-	db.
+	//todo db setting
+	
+	if(db.open())
+	{
+            qDebug() << "connected...aws rds mysql db";
+	}
+	else
+	{
+            qDebug() << "not load db server";
+	}
 }
+
+
