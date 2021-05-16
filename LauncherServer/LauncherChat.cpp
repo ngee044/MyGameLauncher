8fc84175b3aacfbdbd69c052d2ef85742d2e597c
ngee044 <mgee044@naver.com>
2021-05-16 22:17:14 +0900

    Chat Server 함수 구현 커밋

--- LauncherServer/LauncherChat.cpp
+++ LauncherServer/LauncherChat.cpp
@@ -1,3 +1,4 @@
+#include <QDebug>
 #include "LauncherChat.h"
 
 LauncherChat::LauncherChat(QObject *parent)
@@ -6,5 +7,32 @@ LauncherChat::LauncherChat(QObject *parent)
 }
 
 LauncherChat::~LauncherChat()
+{
+}
+
+void LauncherChat::incomingConnection(int socketfd)
+{
+	QTcpSocket* client = new QTcpSocket(this); //클라리언트 소켓 새성
+	client->setSocketDescriptor(socketfd); //
+
+	cliendts_.insert(client); //클라이언트 집합에 새로 들어온 클라이언트 추가
+}
+
+void LauncherChat::slotClientFromToServer()
+{
+	QTcpSocket* client = dynamic_cast<QTcpSocket*>(this->sender());
+	while (client->canReadLine()) //데이터 못읽을때까지 계속 읽는다
+	{
+		QString line = QString::fromUtf8(client->readLine());
+		qDebug() << line;
+
+	}
+}
+
+void LauncherChat::slotDisconnected()
+{
+}
+
+void LauncherChat::slotSendUserInfo()
 {
 }
--- LauncherServer/LauncherChat.h
+++ LauncherServer/LauncherChat.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QMap>
+#include <QSet>
 
 class LauncherChat : public QObject
 {
@@ -9,4 +13,17 @@ class LauncherChat : public QObject
 public:
 	LauncherChat(QObject *parent);
 	~LauncherChat();
+
+public slots:
+	void slotClientFromToServer(); //클라로부터 메시지 전달
+	void slotDisconnected(); //클라 접속 끊길때
+	void slotSendUserInfo(); //새로 들어온 유저 알림
+
+protected:
+	void incomingConnection(int socketfd); //처음 유저가 서버에 접속을 시도할때 
+
+private:
+	int user_amount_; //user number
+	QSet<QTcpSocket*> cliendts_;
+	QMap<QTcpSocket*, QString> users_id; 
 };
